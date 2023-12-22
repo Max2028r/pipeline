8d485c6e8e80a82e06629444758c7f3ff28ef886
Max2028r <107916929+Max2028r@users.noreply.github.com>
2023-12-22 16:49:26 +0200

    Update testing.py

--- testing.py
+++ testing.py
@@ -9,7 +9,7 @@ class TestSumOfArithmeticProgression(unittest.TestCase):
     def test_sum_of_arithmetic_progression(self):
         self.assertEqual(sum_of_arithmetic_progression(5, 3, 7), 98) 
         self.assertEqual(sum_of_arithmetic_progression(0, 1, 10), 45)  
-        self.assertEqual(sum_of_arithmetic_progression(100, 0, 3), 150) 
+        self.assertEqual(sum_of_arithmetic_progression(100, 0, 3), 300) 
         self.assertEqual(sum_of_arithmetic_progression(1, 2, 5), 35)  
 
 if __name__ == '__main__':
