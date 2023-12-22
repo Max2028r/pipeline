510a3693e33f5fad15db6cb1d861023ef3612c93
Max2028r <107916929+Max2028r@users.noreply.github.com>
2023-12-22 16:34:11 +0200

    Update testing.py
    update test

--- testing.py
+++ testing.py
@@ -1,21 +1,16 @@
+import unittest
+
 def sum_of_arithmetic_progression(a_1, d, n):
     sum_n = (n / 2) * (2 * a_1 + (n - 1) * d)
     return sum_n
 
+class TestSumOfArithmeticProgression(unittest.TestCase):
 
+    def test_sum_of_arithmetic_progression(self):
+        self.assertEqual(sum_of_arithmetic_progression(5, 3, 7), 119)
+        self.assertEqual(sum_of_arithmetic_progression(0, 1, 10), 45)
+        self.assertEqual(sum_of_arithmetic_progression(100, 0, 3), 300)
+        self.assertEqual(sum_of_arithmetic_progression(1, 2, 5), 35)
 
-def test_sum_of_arithmetic_progression():
-   
-    assert sum_of_arithmetic_progression(5, 3, 7) == 119
-
-   
-    assert sum_of_arithmetic_progression(0, 1, 10) == 45
-
-   
-    assert sum_of_arithmetic_progression(100, 0, 3) == 300
-
-
-
-
-
-test_sum_of_arithmetic_progression()
+if __name__ == '__main__':
+    unittest.main()
